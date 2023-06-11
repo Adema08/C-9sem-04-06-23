3d94b195e4a6f9fb9e3d8987661b68917fa80591
Adema <aatygaeva08@gmail.com>
2023-06-11 20:05:52 +0600

    1.от 1 до n 2.от m до n 3.сумма цифр числа 4.возв в степень

---
+++ task 67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int DigitalSum(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    return n % 10 + DigitalSum(n / 10);
+}
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DigitalSum(n));
